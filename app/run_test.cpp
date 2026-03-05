eabb5ce8cfe4b6791d851ca23a437387d906d093
realxie <jinyun2007@126.com>
2026-03-05 23:50:36 +0800

    fix search path issues.

--- app/run_test.cpp
+++ app/run_test.cpp
@@ -20,6 +20,7 @@ int main(int argc, char** argv) {
         } else {
             std::vector<std::string> searchPaths = {
                 ".",
+                "..",
                 "scripts",
                 "../scripts",
                 "../../scripts",
